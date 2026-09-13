5cd5689cad22087c786e1800835a8be96bb59705
Dominique Boeckli <dboeckli@gmail.com>
2026-09-13 22:32:53 +0200

    chore: Runner + Renovate
    - clear docker/deploy-k8s/uninstall-k8s: ShConfigurationType -> PowerShellRunType
    - neuer test-k8s-Runner (helm test); tgz-Filter spring-6-reactive-examples-chart-*.tgz
    - Renovate: ignorePaths + shfmt-Custom-Manager
    
    Refs #72

---
+++ .run/scripts/test-k8s.ps1
@@ -0,0 +1,9 @@
+cd target/helm/repo
+
+$file = Get-ChildItem -Filter spring-6-reactive-examples-chart-*.tgz | Select-Object -First 1
+$APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
+Write-Host "test application: $APPLICATION_NAME"
+helm test $APPLICATION_NAME --namespace spring-6-reactive-examples --logs
+
+kubectl delete pod -n spring-6-reactive-examples --field-selector=status.phase==Succeeded
+kubectl delete pod -n spring-6-reactive-examples --field-selector=status.phase==Failed
